9a44f3bcdbe2e0c4ef0c0dfb9314130f67389e1e
Stefan Biedenstein <mbiedens@gmail.com>
2015-10-08 23:29:01 +0200

    mogliBase 3.04

---
+++ deploy.bat
@@ -0,0 +1,10 @@
+@echo "mogliBase.lua"
+@xcopy src\mogliBase.lua ..\AutoCombine\zzzAutoCombine /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\AutoTractor\zzzAutoTractor /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\CutterAttacherHeight\ZZZ_cutterAttacher /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\GearboxAddon\zzzMrGearboxAddon /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\KeyboardSteer\zzzKeyboardSteer /R/U/Y/Q
+
+@echo "mogliHud.lua"
+@xcopy src\mogliHud.lua ..\AutoCombine\zzzAutoCombine /R/U/Y/Q
+@xcopy src\mogliHud.lua ..\AutoTractor\zzzAutoTractor /R/U/Y/Q

355ac46dda3487772b16f9e631b55665ac77396b
Stefan Biedenstein <mbiedens@gmail.com>
2016-02-28 11:00:40 +0100

    mogliBase 3.06
    - global field type degree

--- deploy.bat
+++ deploy.bat
@@ -4,6 +4,7 @@
 @xcopy src\mogliBase.lua ..\CutterAttacherHeight\ZZZ_cutterAttacher /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\GearboxAddon\zzzMrGearboxAddon /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\KeyboardSteer\zzzKeyboardSteer /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\TrailerAssist\ZZZ_trailerAssist /R/U/Y/Q
 
 @echo "mogliHud.lua"
 @xcopy src\mogliHud.lua ..\AutoCombine\zzzAutoCombine /R/U/Y/Q

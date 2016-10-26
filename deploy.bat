f629cb54e3867139b0660dca19caca50dc2e2dd1
Stefan Biedenstein <mbiedens@gmail.com>
2016-10-26 22:55:50 +0200

    v3.10

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,11 @@
 @echo "mogliBase.lua"
-@xcopy src\mogliBase.lua ..\AutoCombine\zzzAutoCombine /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\AutoTractor\zzzAutoTractor /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\AutoCombine\src /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\AutoTractor\src /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\CutterAttacherHeight\ZZZ_cutterAttacher /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\GearboxAddon\zzzMrGearboxAddon /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\GearboxAddon\src /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\KeyboardSteer\zzzKeyboardSteer /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\TrailerAssist\ZZZ_trailerAssist /R/U/Y/Q
 
 @echo "mogliHud.lua"
-@xcopy src\mogliHud.lua ..\AutoCombine\zzzAutoCombine /R/U/Y/Q
-@xcopy src\mogliHud.lua ..\AutoTractor\zzzAutoTractor /R/U/Y/Q
+@xcopy src\mogliHud.lua ..\AutoCombine\src /R/U/Y/Q
+@xcopy src\mogliHud.lua ..\AutoTractor\src /R/U/Y/Q

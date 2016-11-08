b868452d4615d2184d24e872342c851fb3a77a50
Stefan Biedenstein <mbiedens@gmail.com>
2016-11-08 22:17:16 +0100

    3.11
    postLoad calls loadAttributesAndNodes

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,7 @@
 @echo "mogliBase.lua"
-@xcopy src\mogliBase.lua ..\AutoCombine\src /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\AutoTractor\src /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\CutterAttacherHeight\ZZZ_cutterAttacher /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\AIVehicleExtension\src /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\GearboxAddon\src /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\KeyboardSteer\zzzKeyboardSteer /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\TrailerAssist\ZZZ_trailerAssist /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\KeyboardSteer\FS17_KeyboardSteer /R/U/Y/Q
 
 @echo "mogliHud.lua"
-@xcopy src\mogliHud.lua ..\AutoCombine\src /R/U/Y/Q
-@xcopy src\mogliHud.lua ..\AutoTractor\src /R/U/Y/Q
+@xcopy src\mogliHud.lua ..\AIVehicleExtension\src /R/U/Y/Q

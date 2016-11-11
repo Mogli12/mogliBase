4208396b72d5a9d0bbfe4dbffa616ec4cc574bf7
Stefan Biedenstein <mbiedens@gmail.com>
2016-11-11 19:21:40 +0100

    3.12
    GetUiScale() - first version

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,9 @@
 @echo "mogliBase.lua"
 @xcopy src\mogliBase.lua ..\AIVehicleExtension\src /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\GearboxAddon\src /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\CutterAttacherHeight\FS17_CutterAttacher /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\KeyboardSteer\FS17_KeyboardSteer /R/U/Y/Q
+@xcopy src\mogliBase.lua ..\TrailerAssist\FS17_TrailerAssist /R/U/Y/Q
 
 @echo "mogliHud.lua"
 @xcopy src\mogliHud.lua ..\AIVehicleExtension\src /R/U/Y/Q

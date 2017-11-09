01e2d77fe16796ce84061a3ebb151aaeeb99450b
Stefan Biedenstein <mbiedens@gmail.com>
2017-11-09 17:22:36 +0100

    mogliScreen.lua v1.03 => force to show cursor

--- src/mogliScreen.lua
+++ src/mogliScreen.lua
@@ -8,7 +8,7 @@
 -- Usage:  source(Utils.getFilename("mogliScreen.lua", g_currentModDirectory));
 --         _G[g_currentModDirectory.."mogliScreen"].newClass( "AutoCombine", "acParameters" )
 
-local mogliScreenVersion   = 1.01
+local mogliScreenVersion   = 1.03
 local mogliScreenClass     = g_currentModName..".mogliScreen"
 
 if _G[mogliScreenClass] ~= nil and _G[mogliScreenClass].version ~= nil and _G[mogliScreenClass].version >= mogliScreenVersion then
@@ -117,12 +117,14 @@ else
 			if type( _newClass_.mogliScreenPostUpdate ) == "function" then
 				_newClass_.mogliScreenPostUpdate( self, dt )
 			end
+			InputBinding.setShowMouseCursor(true)
 		end
 
 	--********************************
 	-- onOpen
 	--********************************
 		function _newClass_:onOpen()
+			_newClass_:superClass().onOpen(self)
 			g_currentMission.isPlayerFrozen = true
 			if self.vehicle == nil then
 				print("Error: vehicle is empty")
@@ -182,9 +184,10 @@ else
 			if self.pageStateBox ~= nil then
 				self:setPageStates()
 				self:updatePageState()
-			end	
+			end				
 			
-			_newClass_:superClass().onOpen(self)
+			self.mogliShowMouseCursor = InputBinding.getShowMouseCursor()
+			InputBinding.setShowMouseCursor(true)
 		end
 
 	--********************************
@@ -241,6 +244,7 @@ else
 	-- onClose
 	--********************************
 		function _newClass_:onClose()
+			InputBinding.setShowMouseCursor(self.mogliShowMouseCursor)
 			if type( _newClass_.mogliScreenOnClose ) == "function" then
 				_newClass_.mogliScreenOnClose( self )
 			end

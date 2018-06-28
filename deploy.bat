af6f5f4e5b07d519860e9567f472cb25346f4fdd
Stefan Biedenstein <mbiedens@gmail.com>
2018-06-28 14:09:59 +0200

    v3.20
    Avoid message "WARNING: undefined input in gearboxMogli:  "

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,6 @@
 @echo "mogliBase.lua"
 @xcopy src\mogliBase.lua ..\AIVehicleExtension\src /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\GearboxAddon\src /R/U/Y/Q
-@xcopy src\mogliBase.lua ..\CutterAttacherHeight\FS17_CutterAttacher /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\KeyboardSteer\FS17_KeyboardSteer /R/U/Y/Q
 @xcopy src\mogliBase.lua ..\TrailerAssist\FS17_TrailerAssist /R/U/Y/Q
 
